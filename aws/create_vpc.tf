9024e9fa54bfaa4f934417a3a7bdba3b9f6678dd
erpost <25180070+erpost@users.noreply.github.com>
2021-02-05 15:54:49 -0500

    Update

--- aws/create_vpc.tf
+++ aws/create_vpc.tf
@@ -4,9 +4,9 @@ provider "aws" {
 
 }
 
-#Create VPC
-resource "aws_vpc" "vpc" {
-  cidr_block = "10.1.0.0/16"
+# Create VPC
+resource "aws_vpc" "vpc_terraform" {
+  cidr_block = "10.0.0.0/16"
 
   tags = {
     Name = "VPC-Terraform"
@@ -14,3 +14,66 @@ resource "aws_vpc" "vpc" {
     Owner = "erpost"
   }
 }
+# Create Private Subnet
+resource "aws_subnet" "subnet1_private" {
+  vpc_id     = aws_vpc.vpc_terraform.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Sub1Private-Terraform"
+    Group = "ICO"
+    Owner = "erpost"
+  }
+}
+# Create Public Subnet
+resource "aws_subnet" "subnet2_public" {
+  vpc_id     = aws_vpc.vpc_terraform.id
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Sub2Public-Terraform"
+    Group = "ICO"
+    Owner = "erpost"
+  }
+}
+# Remove Default Security Group Rules (No access)
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.vpc_terraform.id
+
+  ingress {
+    protocol  = -1
+    self      = false
+    from_port = 0
+    to_port   = 0
+  }
+}
+# Create Security Group
+resource "aws_security_group" "allow_https" {
+  name        = "allow_https"
+  description = "Allow HTTPS inbound traffic"
+  vpc_id      = aws_vpc.vpc_terraform.id
+
+  ingress {
+    description = "HTTPS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.vpc_terraform.cidr_block, "142.112.43.72/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Group = "ICO"
+    Owner = "erpost"
+  }
+}
+# Create Internet Gateway
+
+# Create Public Route Table
